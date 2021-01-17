3c1e5e41660923c7db9d34b059448cf306cb503c
Parker Dell <parker.dell@maxar.com>
2021-01-17 15:35:48 -0500

    added new style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ConnectItemComponent } from './connect/connect-item/connect-item.component';
-import { ConnectComponent } from './connect/connect.component';
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
+import { BidsComponent } from './bids/bids.component';
 import { LandingComponent } from './landing/landing.component';
-import { LibraryComponent } from './library/library.component';
-import { MenComponent } from './ministries/men/men.component';
-import { MinistriesComponent } from './ministries/ministries.component';
-import { PreschoolComponent } from './ministries/preschool/preschool.component';
-import { ServeComponent } from './ministries/serve/serve.component';
-import { YouthComponent } from './ministries/youth/youth.component';
-import { NativityComponent } from './nativity/nativity.component';
-import { SermonsComponent } from './sermons/sermons.component';
-import { StaffComponent } from './staff/staff.component';
+import { EmergencyComponent } from './services/emergency/emergency.component';
+import { WaterComponent } from './services/water/water.component';
+
 
 const routes: Routes = [
-  { path: 'sermons', component: SermonsComponent},
-  { path: 'nativity', component: NativityComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'library', component: LibraryComponent},
   { path: '', component: LandingComponent},
-  { path: 'contact', component: ContactComponent},
-  { path: 'staff', component: StaffComponent},
-  { path: 'ministries', component: MinistriesComponent},
-  { path: 'ministries/preschool', component: PreschoolComponent},
-  { path: 'ministries/youth', component: YouthComponent},
-  { path: 'ministries/men', component: MenComponent},
-  { path: 'ministries/serve', component: ServeComponent},
-  { path: 'connect', component: ConnectComponent},
-  { path: 'connect/:slug', component: ConnectItemComponent},
+  { path: 'services/water', component: WaterComponent},
+  { path: 'services/emergency', component: EmergencyComponent},
+  { path: 'bids', component: BidsComponent},
   
 
 ];
